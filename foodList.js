72b3704b832759143f22ef5cc7d51b268c025753
KirillKulakow <53854091+KirillKulakow@users.noreply.github.com>
2020-03-01 20:30:17 +0200

    js

---
+++ foodList.js
@@ -0,0 +1,57 @@
+const dishList = [
+    {dishName: "Минестроне",
+    dishSelectCategory: "Первые блюда",
+    dishImg: "https://demo.joinposter.com/upload/4/menu/product_1406456449_170.jpg",
+    dishSelfPrice: 43.42,
+    dishExtraCharge: 173,
+    dishFinalPrice: 118.54,
+    ingredientsArray: [
+        {
+            ingredient: "Лимон",
+            ingredientGross: 128,
+            ingerdientNett: 100,
+            ingredientFirstPrice: 2.68
+        },
+        {
+            ingredient: "Укроп",
+            ingredientGross: 59,
+            ingerdientNett: 50,
+            ingredientFirstPrice: 2.18
+        },
+        {
+            ingredient: "Оливковое масло",
+            ingredientGross: 40,
+            ingerdientNett: 40,
+            ingredientFirstPrice: 1.68
+        }
+        ]
+    },
+    {dishName: "Минестроне",
+    dishSelectCategory: "Первые блюда",
+    dishImg: "https://demo.joinposter.com/upload/4/menu/product_1406456449_170.jpg",
+    dishSelfPrice: 43.42,
+    dishExtraCharge: 173,
+    dishFinalPrice: 118.54,
+    ingredientsArray: [
+        {
+            ingredient: "Лимон",
+            ingredientGross: 128,
+            ingerdientNett: 100,
+            ingredientFirstPrice: 2.68
+        },
+        {
+            ingredient: "Укроп",
+            ingredientGross: 59,
+            ingerdientNett: 50,
+            ingredientFirstPrice: 2.18
+        },
+        {
+            ingredient: "Оливковое масло",
+            ingredientGross: 40,
+            ingerdientNett: 40,
+            ingredientFirstPrice: 1.68
+        }
+        ]
+    }
+]
+export { dishList }
